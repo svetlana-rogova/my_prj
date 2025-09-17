12f92cd3605b4f5a09a0f5cbba3c9ab1221e73b0
Светлана Рогова <mifistofiya@gmail.com>
2025-09-17 23:01:36 +0300

    refinement of the function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -5,14 +5,20 @@ def mask_account_card(info: str) -> str:
     """Функция для обработки информации как о картах, так и о счетах"""
     new_info = info.split()
     name_info = [el for el in new_info if el.isalpha()]
+    number = new_info[-1]
     if new_info[0] == "Счет":
-        return " ".join(name_info) + " " + get_mask_account(new_info[-1])
+        if len(number) <= 4 or not number.isdigit():
+            return "В номере счета должно быть больше 4 цифр"
+        masked = get_mask_account(number)
     else:
-        return " ".join(name_info) + " " + get_mask_card_number(new_info[-1])
+        if len(number) != 16 or not number.isdigit():
+            return "Номер карты должен состоять из 16 цифр"
+        masked = get_mask_card_number(number)
+    return " ".join(name_info) + " " + masked
 
 
 if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
 
 
 def get_date(time_data: str) -> str:
