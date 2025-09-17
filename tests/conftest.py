441e1ac9f07209fe8713ab936efde0752f907a3e
Светлана Рогова <mifistofiya@gmail.com>
2025-09-17 21:24:12 +0300

    creating test_sort_by_date

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,3 +29,4 @@ def enumeration_dictionaries():
          {"id": "594226727", "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
          {"id": "615064591", "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
+
