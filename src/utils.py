a4e65191f582ea0c31abcdd2b83f606d64deafa9
Светлана Рогова <mifistofiya@gmail.com>
2025-10-15 23:34:40 +0300

    added logs for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,29 @@
 import json
+import logging
 import os
 
 
 def data_transactions(input_file):
     """Функция для чтения файла и вывода списка с данными о транзакциях"""
-    if not os.path.exists(input_file) or os.path.getsize(input_file) == 0:
-        return []
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler("../logs/utils.log", "w", encoding="utf-8")
+    file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s %(message)s")
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.DEBUG)
     try:
+        if os.path.getsize(input_file) == 0:
+            logger.debug("Файл пуст")
+            return []
         with open(input_file, "r", encoding="utf-8") as f:
             transactions = json.load(f)
+            logger.info("Файл прочитан и данные выведены в консоль")
             return transactions
     except (FileNotFoundError, json.JSONDecodeError):
-            return []
-
+        logger.error("Фaйл не существует или данные в файле не являются корректным документом JSON")
+        return []
 
 
 input_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
-print(data_transactions(input_file))
+if __name__ == "__main__":
+    print(data_transactions(input_file))
